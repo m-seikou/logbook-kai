df7adba8d8deeab85da3b32ebcf8bed0dfcdb46b
m-seikou <angelan@mihoshi.info>
2023-03-13 12:00:58 +0900

    2023早春イベント札対応

--- src/main/java/logbook/internal/SeaArea.java
+++ src/main/java/logbook/internal/SeaArea.java
@@ -6,16 +6,17 @@ import java.util.stream.Stream;
  * 海域
  */
 public enum SeaArea {
-    識別札1("前路掃討部隊", 1, 4),
-    識別札2("遠征偵察部隊", 2, 8),
-    識別札3("遠征艦隊先遣隊", 3, 10),
-    識別札4("地中海連合艦隊", 4, 12),
-    識別札5("トーチ作戦英軍部隊", 5, 14),
-    識別札6("トーチ作戦派遣部隊", 6, 16),
-    識別札7("中央任務部隊", 7, 18),
-    識別札8("東方任務部隊", 8, 20),
-    識別札9("西方先遣部隊", 9, 22),
-    識別札10("西方任務決戦部隊", 10, 5);
+
+    識別札1("横須賀防備戦隊", 1, 4),
+    識別札2("第二水雷戦隊", 2, 6),
+    識別札3("支援連合艦隊", 3, 8),
+    識別札4("連合艦隊", 4, 10),
+    識別札5("空母機動部隊", 5, 12),
+    識別札6("unknown", 6, 16),
+    識別札7("unknown", 7, 18),
+    識別札8("unknown", 8, 20),
+    識別札9("unknown", 9, 22),
+    識別札10("unknown", 10, 5);
 
     /**
      * 名前
@@ -28,6 +29,7 @@ public enum SeaArea {
     private final int area;
 
     private final int imageIndex;
+
     SeaArea(String name, int area, int imageIndex) {
         this.name = name;
         this.area = area;
